3dd882b62de0e19e16a0487132685ac98ebb6d4b
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-22 23:59:16 +0300

    openvidu tutorial

--- src/main/java/com/dhr/controllers/RecorderApp.java
+++ src/main/java/com/dhr/controllers/RecorderApp.java
@@ -1,13 +1,13 @@
 package com.dhr.controllers;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecorderApp
-{
-	public static void main( String[] args )
-    {
-    	SpringApplication.run(RecorderApp.class, args);
+@EnableAutoConfiguration
+public class RecorderApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RecorderApp.class, args);
     }
 }
