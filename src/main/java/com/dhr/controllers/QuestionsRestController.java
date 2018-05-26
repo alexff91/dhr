2d20405ca53ba09971e7612bc2727a0a47107a3b
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-27 02:04:06 +0300

    update questions controller

--- src/main/java/com/dhr/controllers/QuestionsRestController.java
+++ src/main/java/com/dhr/controllers/QuestionsRestController.java
@@ -3,8 +3,13 @@ package com.dhr.controllers;
 import com.dhr.model.Question;
 import com.dhr.services.QuestionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +31,17 @@ public class QuestionsRestController {
     @GetMapping("/{vacancyId}")
     public List<Question> getQuestionsByVacancy(@PathParam("vacancyId") Long id) {
         return questionService.getAllByVacancy(id);
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody Question question) {
+        questionService.save(question);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{questionId}")
+    public ResponseEntity delete(@PathParam("questionId") Long questionId) {
+        questionService.delete(questionService.get(questionId).get());
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
