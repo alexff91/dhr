aa1023fb0610089b3dbfab503111df9418ecf707
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-27 01:35:19 +0300

    run command

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-mkdir -p /browser-recordings/user/
+mvn clean package
+docker build -t demo .
+docker stop demo
+docker run -p 443:5443 --restart=always --name=record -d -v /browser-recordings:/browser-recordings/ demo
