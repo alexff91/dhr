2c4544c3d427ed1633d482e8d8ce54a03d526ba1
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-05-30 20:25:01 +0300

    add equals and hashcode

--- src/main/java/com/dhr/model/QuestionRespond.java
+++ src/main/java/com/dhr/model/QuestionRespond.java
@@ -2,6 +2,7 @@ package com.dhr.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import java.util.Date;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 @Builder
 @Document(collection = "questions-responds")
 public class QuestionRespond {
