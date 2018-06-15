54c62e42908564d738d3f42025745eb6e13c1497
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-06-15 14:57:56 +0300

    fix respond review controller

--- src/main/java/com/dhr/controllers/respond/RespondReviewRestController.java
+++ src/main/java/com/dhr/controllers/respond/RespondReviewRestController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/v1/vacancies/{vacancyId}/responds")
+@RequestMapping("/api/v1/vacancies/{vacancyId}/responds/{respondId}/review")
 public class RespondReviewRestController {
     @Autowired
     RespondFeedbackService respondFeedbackService;
