27bab1f7a7e21857c7e5ef09f0c1dcff06ef7cf2
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-06-16 11:58:20 +0300

    fix docker file

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 mvn clean package
-docker build -t demo2 .
-docker stop record2
-docker rm record2
-docker run -p 8082:8082 --privileged=true --network host --restart=always --name=record2 -d -v /browser-recordings:/browser-recordings/ -v /etc/letsencrypt/live/vi-hr.com/:/etc/letsencrypt/live/vi-hr.com/ demo2
+docker build -t demo .
+docker stop record
+docker rm record
+docker run -p 8082:8082 --privileged=true --network host --restart=always --name=record -d -v /browser-recordings:/browser-recordings/ -v /etc/letsencrypt/live/app.vi-hr.com/:/etc/letsencrypt/live/app.vi-hr.com/ demo
