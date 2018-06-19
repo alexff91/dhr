8d2eaaf846ac60aa08895e21528683b7e12e8dd9
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-06-20 00:29:22 +0300

    fix

--- src/main/java/com/dhr/model/Respond.java
+++ src/main/java/com/dhr/model/Respond.java
@@ -45,7 +45,6 @@ public class Respond {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "vacancy_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
     private Vacancy vacancy;
 
