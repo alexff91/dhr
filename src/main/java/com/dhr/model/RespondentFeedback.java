f6e4caa709e0e05383ce12004dcaf0c2c71c74b9
Aleksandr.Fedorov <aleksandr.fedorov@billing.ru>
2018-07-05 01:20:00 +0300

    fix

--- src/main/java/com/dhr/model/RespondentFeedback.java
+++ src/main/java/com/dhr/model/RespondentFeedback.java
@@ -7,9 +7,12 @@ import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -28,6 +31,8 @@ public class RespondentFeedback {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "respond_id", nullable = false)
     @JsonIgnore
     Respond respond;
 
